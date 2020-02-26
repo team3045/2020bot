e1c97ef816c2d3b0ca922ef22892be5c5fd1a5a4
allanstewart <46907590+allanstewart@users.noreply.github.com>
2020-02-25 19:35:04 -0800

    Update Robot.java
    @apstewar @Billy we attempted to write code for the shooter and magazine, but it would not move the motors. The code says that there is no errors, the talons all have the same software, the talons show up in Phoenix Tuner, the joysticks show up in Driver Station, and the talons flash in unison. Could you please take a look at this? Holman wrote the code and I helped deploy it and checked the talons. Thank you!

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -36,6 +36,7 @@ public class Robot extends TimedRobot {
 
   private final Joystick leftJoystick = new Joystick(0);
   private final Joystick rightJoystick = new Joystick(1);
+  private final Joystick shooterJoystick = new Joystick(2);
 
   /*@Override
   public boolean get() {
@@ -44,7 +45,7 @@ public class Robot extends TimedRobot {
   }
 
   ;{
-
+    
   @Override 
   public Void enable() {
       // TODO Auto-generated method stub
@@ -57,30 +58,29 @@ public class Robot extends TimedRobot {
 
   }}; */
 
-  private WPI_TalonSRX rightTankMotor1Controller = new WPI_TalonSRX(1);
-  private WPI_TalonSRX rightTankMotor2Controller = new WPI_TalonSRX(2);
-  private WPI_TalonSRX leftTankMotor1Controller = new WPI_TalonSRX(3);
-  private WPI_TalonSRX leftTankMotor2Controller = new WPI_TalonSRX(4);
-  private WPI_TalonSRX intakeAxelController = new WPI_TalonSRX(5);
-  private WPI_TalonSRX magazineMiddleController = new WPI_TalonSRX(6);
-  private WPI_TalonSRX shooterLeftSide = new WPI_TalonSRX(7);
-  private WPI_TalonSRX shooterRightSide = new WPI_TalonSRX(8);
-  private WPI_TalonSRX winchController = new WPI_TalonSRX(9);
+  private final WPI_TalonSRX rightTankMotor1Controller = new WPI_TalonSRX(1);
+  private final WPI_TalonSRX rightTankMotor2Controller = new WPI_TalonSRX(2);
+  private final WPI_TalonSRX leftTankMotor1Controller = new WPI_TalonSRX(3);
+  private final WPI_TalonSRX leftTankMotor2Controller = new WPI_TalonSRX(4);
+  private final WPI_TalonSRX intakeAxelController = new WPI_TalonSRX(5);
+  private final WPI_TalonSRX magazineMiddleController = new WPI_TalonSRX(6);
+  private final WPI_TalonSRX shooterLeftSide = new WPI_TalonSRX(7);
+  private final WPI_TalonSRX shooterRightSide = new WPI_TalonSRX(8);
+  private final WPI_TalonSRX winchController = new WPI_TalonSRX(9);
 
   private int practiceEncoderPos = 0;
+
   private enum PracticeEncodeState {
-    DISABLE,
-    MOVE,
-    CHANGE,
-    WAIT,
+    DISABLE, MOVE, CHANGE, WAIT,
   }
+
   private PracticeEncodeState encodeState = PracticeEncodeState.DISABLE;
-  private Timer moveTimer = new Timer();
+  private final Timer moveTimer = new Timer();
 
 
   /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
    */
   @Override
   public void robotInit() {
@@ -88,16 +88,17 @@ public class Robot extends TimedRobot {
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
 
-    rightTankMotor1Controller.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0); 
+    rightTankMotor1Controller.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
@@ -105,14 +106,15 @@ public class Robot extends TimedRobot {
 
   /**
    * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString line to get the
+   * auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure below with additional strings. If using the SendableChooser
+   * make sure to add them to the chooser code above as well.
    */
   @Override
   public void autonomousInit() {
@@ -127,13 +129,13 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    case kCustomAuto:
+      // Put custom auto code here
+      break;
+    case kDefaultAuto:
+    default:
+      // Put default auto code here
+      break;
     }
   }
 
@@ -142,9 +144,9 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    if(leftJoystick.getRawButton(2)){
+    if (leftJoystick.getRawButton(2)) {
       practiceSetMove();
-    }else{
+    } else {
       tankDrive();
     }
     intake();
@@ -161,13 +163,13 @@ public class Robot extends TimedRobot {
   public void tankDrive() {
     final double percentOutput = .7;
 
-    double leftPower = leftJoystick.getRawAxis(vertical);
-    double rightPower = rightJoystick.getRawAxis(vertical);
+    final double leftPower = leftJoystick.getRawAxis(vertical);
+    final double rightPower = rightJoystick.getRawAxis(vertical);
 
-    leftTankMotor1Controller.set(leftPower * percentOutput*-1);
+    leftTankMotor1Controller.set(leftPower * percentOutput * -1);
     rightTankMotor1Controller.set(rightPower * percentOutput);
     leftTankMotor2Controller.set(ControlMode.Follower, 3);
-    rightTankMotor2Controller.set(ControlMode.Follower,1);
+    rightTankMotor2Controller.set(ControlMode.Follower, 1);
 
   }
 
@@ -180,12 +182,32 @@ public class Robot extends TimedRobot {
   }
 
   public void printRPMs() {
-    double rightRPM = rightTankMotor1Controller.getSelectedSensorVelocity();
+    final double rightRPM = rightTankMotor1Controller.getSelectedSensorVelocity();
     if (count++ % 20 == 0) {
       System.err.println("Right side = " + rightRPM + " RPM");
       count = 1;
       System.err.println("Practice encoder position: " + practiceEncoderPos);
     }
+  }
+
+  public void shooter() {
+    if(shooterJoystick.getRawButton(1)){
+      shooterLeftSide.set(1.0);
+      shooterRightSide.set(-1.0);
+    }
+    else{
+      shooterRightSide.set(0.0);
+      shooterLeftSide.set(0.0);
+    }
+  }
+
+  public void magazine(){
+      if(shooterJoystick.getRawButton(2)){
+        magazineMiddleController.set(.5);
+      }
+      else{
+        magazineMiddleController.set(0.0);
+      }
   }
   public void practiceSetMove()
   {
